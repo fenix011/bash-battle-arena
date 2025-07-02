48dcbacc7c98ba2693a2c2c3974d52f710b6fc0c
YasinYS <yasinhirsi00@gmail.com>
2025-07-02 02:03:40 +0100

    Removed solutions folder from repo and added to .gitignore

--- solutions/count_lines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-  echo "No file provided"
-else
-  wc -l < "$1"
-fi
-
--- solutions/hero_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -f Arena/hero.txt ]];
-then
-	echo "Hero found!"
-else
-	echo "Hero missing!"
-fi
